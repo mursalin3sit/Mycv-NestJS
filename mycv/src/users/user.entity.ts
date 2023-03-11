b6cfb4de6d59611913be8d912afdce034d6a6d3e
Mursalin <mursalin3sit@gmail.com>
2023-03-11 11:24:31 +0600

    Update

---
+++ mycv/src/users/user.entity.ts
@@ -0,0 +1,28 @@
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Update User with id', this.id)
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove User with id', this.id)
+  }
+}
